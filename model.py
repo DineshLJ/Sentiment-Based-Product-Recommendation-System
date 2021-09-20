cbb2393669e9bb8d10ffd37d693a62e46358e50a
DineshLJ <70759091+DineshLJ@users.noreply.github.com>
2021-09-20 21:26:56 +0530

    Update model.py

--- model.py
+++ model.py
@@ -6,21 +6,31 @@ import pandas as pd
 
 def getProducts():
     try:
+        #To remove white space
         username = username.strip()
+        org_username = username
+        username = org_username.lower()
         user = request.form['username']
+        #Loading required pickle files
         mapping = pickle.load(open('./pickle/dataset_final.csv', 'rb'))
         user_rating = pickle.load(open('./pickle/user_final_rating.pkl', 'rb'))
         word_vectorizer = pickle.load(open('./pickle/Vectorizer.pkl', 'rb'))
+        model = pickle.load(open('./pickle/final_model.pkl', 'rb'))
+        #Top20 products recommendation
+        
         top_20_products = user_rating.loc[user].sort_values(ascending=False)[0:20]
         df = pd.merge(top_20_products, mapping, left_on='name', right_on='name', how='left')
-        model = pickle.load(open('./pickle/final_model.pkl', 'rb'))
+        
         reviews = df['reviews_text'].values.astype('U')
+        #word_vectorizer
         reviews_transformed = word_vectorizer.transform(reviews.tolist())
+        #Model Prediction
         pred_val = model.predict(reviews_transformed)
         df['user_sentiment'] = pred_val
         pro = df.groupby('name')['user_sentiment'].mean()
         pro = pro.reset_index()
         top_5_products = pro.sort_values(by='user_sentiment', ascending=False)[0:5]
+        #Top5 products based on sentiment
         print(top_5_products)
         return render_template('results.html',products = top_5_products['name'] , page="result")
     except:
